ecd102b2dae04736a5c149039a8113ca9a2bf022
Amil Sajeev <amilsajeev@gmail.com>
2020-02-11 23:39:52 +0530

    organize code

---
+++ constants.js
@@ -0,0 +1,10 @@
+// months
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+// project Types
+const projectTypes = {
+    CROWDFUNDING: 'Crowdfunding',
+    SOCIALGIVING: 'Social Giving',
+    PROMOTIONS: 'Promotions',
+    SOCIALCHANGE: 'Social Change'
+}
