70abdb1feaa09a81bcc3c1565eabcb2d184a9117
Amil Sajeev <amilsajeev@gmail.com>
2020-02-11 23:28:17 +0530

    move update method to profile.js

--- profile.js
+++ profile.js
@@ -27,3 +27,30 @@ $(document).ready(function () {
 $('#dateOfBirth').datepicker({
     uiLibrary: 'bootstrap4'
 });
+
+function updateProfile() {
+    let editProfileFormValues = {};
+    $.each($('#editProfileForm').serializeArray(), function (i, field) {
+        editProfileFormValues[field.name] = field.value;
+    });
+    // update leftpanel data models with new value
+    userDetails.userIntroDetails = editProfileFormValues.userDetailInto !== "" ?
+        editProfileFormValues.userDetailInto : userDetails.userIntroDetails;
+    userDetails.birthDay = editProfileFormValues.dateOfBirth !== "" ?
+        convertDateIntoDDMMMYYYY(editProfileFormValues.dateOfBirth) : userDetails.birthDay;
+    userDetails.location = editProfileFormValues.location !== "" ?
+        editProfileFormValues.location : userDetails.location;
+
+    // update left panel with new values or create left panel with new values
+    LeftPanelCreation();
+    $("#syne-edit-profile").removeClass("open-pop");
+    $("body").removeClass("modal-blur");
+}
+
+
+
+function convertDateIntoDDMMMYYYY(date) {
+    let current_datetime = new Date(date)
+    let formatted_date = current_datetime.getDate() + " " + months[current_datetime.getMonth()] + " " + current_datetime.getFullYear()
+    return formatted_date;
+}
