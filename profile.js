23179365f0a1812ada690e0115edae16ebf9556f
anandhu <aajayakumar@menervasoftware.com>
2020-02-11 13:57:59 +0530

    form

---
+++ profile.js
@@ -0,0 +1,34 @@
+function formValidCheck(form, number) {}
+
+function updateProfile() {
+    $("#syne-edit-profile").removeClass("open-pop");
+    $("body").removeClass("modal-blur");
+}
+
+$(document).ready(function () {
+
+    $(".modal-btn").click(function (e) {
+        //$(this).next("div").addClass("open-pop");
+        var modl_id = $(this).attr('data-target');
+        $("#" + modl_id).addClass("open-pop");
+        $("body").addClass("modal-blur");
+        e.preventDefault();
+    });
+
+    $("#syne-edit-profile .modalclose").click(function (e) {
+        $("#syne-edit-profile").removeClass("open-pop");
+        $("body").removeClass("modal-blur");
+        e.preventDefault();
+    });
+
+    $(".pop-modal").click(function (e) {
+        if (e.target.className != 'pop-modal' && !$('.pop-modal').find(e.target).length) {
+            $(this).removeClass("open-pop");
+            $("body").removeClass("modal-blur");
+        }
+    });
+});
+
+$('#dobPicker').datepicker({
+    uiLibrary: 'bootstrap4'
+});
