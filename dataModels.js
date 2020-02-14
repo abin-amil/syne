942b6f1e4f33c1d68674127faafbf60adb099247
Amil Sajeev <amilsajeev@gmail.com>
2020-02-14 23:15:29 +0530

    Organisation page

--- dataModels.js
+++ dataModels.js
@@ -131,3 +131,142 @@ var userDetails = {
     birthDay: '02 Feb 2012',
     location: 'kochi'
 };
+
+var organisationDashBoardData = {
+    name: 'Vinith Gopal',
+    mailId: '@vinithgopalofficial',
+    location: 'BANGLORE',
+    introDetails: ' My hobbies are breakfast, lunch and dinner topped with a chocolate deserts',
+    profileCompletionPercentage: '80',
+    followersCount: '498',
+    followingCount: '8',
+    coverPhotoUrl: 'img/beautiful-flower-picture-facebook-cover.jpg',
+    profilePhotoUrl: 'img/user.png',
+    leftPanelTabs: [
+        {
+            name: 'Timeline',
+            iconUrl: 'img/time-line.png'
+        },
+        {
+            name: 'About Me',
+            iconUrl: 'img/about-me.png'
+        },
+        {
+            name: 'Project',
+            iconUrl: 'img/project-grey-small.png'
+        },
+        {
+            name: 'Events',
+            iconUrl: 'img/events-grey-small.png'
+        },
+        {
+            name: 'Stories',
+            iconUrl: 'img/stories-grey-small.png'
+        },
+        {
+            name: 'Organisations',
+            iconUrl: 'img/org-grey-small.png'
+        },
+        {
+            name: 'Transactions',
+            iconUrl: 'img/transaction-grey-small.png'
+        },
+        {
+            name: 'Settings & Privacy',
+            iconUrl: 'img/settings-grey-small.png'
+        }
+    ],
+    newsFeedData: [{
+        userImage: 'img/user.png',
+        name: 'Sanne Viscal',
+        email: '@sanneviscal',
+        postedTimeDuration: '1 week',
+        imageUrl: 'img/flood-relief.jpg',
+        descriptionHeading: 'Kerala Flood Relief 2019',
+        descriptionContent: 'World Vision distributed food, clothing and hygiene and household items among families in Kerala whose homes have been damaged or destroyed.',
+        targetAmount: '2,500,000',
+        raisedAmount: '293,145',
+        contributorsCount: '2.3K',
+        completionPercentage: '42',
+        isLikedByMe: 'true',
+        likedCount: '657',
+        shareCount: '1.5K',
+        commentCount: '12',
+        comments: []
+    },
+    {
+        userImage: 'img/user.png',
+        name: 'Honey Sara John',
+        email: '@honeysarajohn',
+        postedTimeDuration: '1 month',
+        imageUrl: 'img/beautiful-flower-picture-facebook-cover.jpg',
+        descriptionHeading: 'The state of ux in 2020',
+        descriptionContent: 'A digital team goes and project become more complex, designers are being valued by collaboration and team enablement rather than only individual tasks.',
+        targetAmount: '',
+        raisedAmount: '',
+        contributorsCount: '',
+        completionPercentage: '',
+        isLikedByMe: 'false',
+        likedCount: '657',
+        shareCount: '0',
+        commentCount: '0',
+        comments: []
+    }
+    ],
+    recommendedProject: [{
+        imageUrl: 'img/autumn-seasonal-sale-black.jpg',
+        projectDuration: '3 months',
+        projectName: 'Digital Democracy',
+        projectContent: 'We aim to create digital democracy, where people and…',
+        targetAmount: '2,500,000',
+        raisedAmount: '293,145',
+        completionPercentage: '42'
+    },
+    {
+        imageUrl: 'img/beautiful-flower-picture-facebook-cover.jpg',
+        projectDuration: '3 months',
+        projectName: 'Digital Democracy',
+        projectContent: 'We aim to create digital democracy, where people and…',
+        targetAmount: '3,123,000',
+        raisedAmount: '123,121',
+        completionPercentage: '75'
+    }
+    ],
+    whoToFollowList: [
+        {
+            name: 'Sanne Viscaal',
+            email: '@sanneviscaal',
+            imageUrl: 'img/user.png',
+            followersCount: '13k',
+            mutualFollwerName: 'Ginu George',
+            mutualFollowersCount: '5'
+        },
+        {
+            name: 'Iruka Akuchi',
+            email: '@irukaakuchi',
+            imageUrl: 'img/user.png',
+            followersCount: '10k',
+            mutualFollwerName: 'Ginu George',
+            mutualFollowersCount: '7'
+        }
+
+    ],
+    recommendedOrganisations: [
+        {
+            name: 'Votonomics Foundation',
+            email: '@votonimics',
+            imageUrl: 'img/user.png',
+            followersCount: '13k',
+            mutualFollwerName: 'Ginu George',
+            mutualFollowersCount: '5'
+        },
+        {
+            name: 'Syne Foundation',
+            email: '@synefoundation',
+            imageUrl: 'img/user.png',
+            followersCount: '10k',
+            mutualFollwerName: 'Ginu George',
+            mutualFollowersCount: '7'
+        }
+    ]
+};
