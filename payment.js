a6ba62138094c5aa621749f3ab6be50b17e15df2
Amil Sajeev <amilsajeev@gmail.com>
2020-02-26 22:54:53 +0530

    payment second page

---
+++ payment.js
@@ -0,0 +1,4 @@
+function proceedToPayButtonClick() {
+    document.getElementById('logegedInUserPaymentPage1').style.display = "none";
+    document.getElementById('logegedInUserPaymentPage2').style.display = "block";
+}
