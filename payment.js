3d53833e3121427911b56151b00b85349129aa07
Amil Sajeev <amilsajeev@gmail.com>
2020-02-27 00:24:22 +0530

    payment successful page

--- payment.js
+++ payment.js
@@ -2,3 +2,9 @@ function proceedToPayButtonClick() {
     document.getElementById('logegedInUserPaymentPage1').style.display = "none";
     document.getElementById('logegedInUserPaymentPage2').style.display = "block";
 }
+
+function proceedToPaymentGateway(){
+    document.getElementById('logegedInUserPaymentPage1').style.display = "none";
+    document.getElementById('logegedInUserPaymentPage2').style.display = "none";
+    document.getElementById('paymentSucessfullPage').style.display = "block";
+}
