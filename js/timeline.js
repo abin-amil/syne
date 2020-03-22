b3dcd7cdbb86ac942e22e3b9942bc038de27402a
Amil Sajeev <amilsajeev@gmail.com>
2020-03-22 21:41:29 +0530

    share popup issues solved

--- js/timeline.js
+++ js/timeline.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
-    setTab(1);
+    setTab(1);    
 });
+
+$("#syne-share-popup .modalclose").click(function (e) {
+    closeModal("#syne-share-popup");
+    e.preventDefault();
+});
+
+$("#syne-share-popup-post").click(function (e) {
+    closeModal("#syne-share-popup");
+    e.preventDefault();
+});
+
+function closeModal(id) {
+    $(id).removeClass("open-pop");
+    $("body").removeClass("modal-blur");
+}
+
