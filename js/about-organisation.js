f9b52a065f26927a64c27723e95962982006ce9a
Abin Alias <abinalyas@gmail.com>
2020-04-13 00:00:05 +0530

    sprint 1 changes

--- js/about-organisation.js
+++ js/about-organisation.js
@@ -29,6 +29,11 @@ function closeModal(id) {
     $(id).removeClass("open-pop");
     $("body").removeClass("modal-blur");
 }
+function closeModals(id1,id2) {
+    $(id1).removeClass("open-pop");
+    $(id2).removeClass("open-pop");
+    $("body").removeClass("modal-blur");
+}
 
 $("#edit-oganisation-popup .modalclose").click(function (e) {
     closeModal("#edit-oganisation-popup");
