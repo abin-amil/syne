bf4a82039a594fb7996c79b20df82fc81061e7f7
Amil Sajeev <amilsajeev@gmail.com>
2020-04-24 15:29:00 +0530

    Left panel common template created and used that in all equied files

--- js/about-organisation.js
+++ js/about-organisation.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    createOrgLeftPanelTabs();
     setTab(2);
 
     $(".modal-btn").click(function (e) {
@@ -10,20 +9,6 @@ $(document).ready(function () {
     });
 });
 
-function createOrgLeftPanelTabs() {
-    if (document.getElementById("template-left-panel-tab-details")) {
-        let leftpanelTab = document.getElementById("template-left-panel-tab-details");
-        let leftPanelTabHtml = leftpanelTab.innerHTML;
-        let leftPanelTabFinalHtml = "";
-        organisationDashboadData.leftPanelTabs.forEach((tab, index) => {
-            leftPanelTabFinalHtml += leftPanelTabHtml.replace(/{{iconUrl}}/g, tab.iconUrl)
-                .replace(/{{tabName}}/g, tab.name)
-                .replace(/{{path}}/g, tab.path)
-                .replace(/{{id}}/g, tab.id);
-        });
-        document.getElementById("org-left-panel-tabs").innerHTML = leftPanelTabFinalHtml;
-    }
-}
 
 function closeModal(id) {
     $(id).removeClass("open-pop");
