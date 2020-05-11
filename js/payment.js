746966295ced8412269c339aa8ffbe7544405afa
Amil Sajeev <amilsajeev@gmail.com>
2020-05-11 21:45:51 +0530

    payment page issue resolved

--- js/payment.js
+++ js/payment.js
@@ -1,3 +1,75 @@
+$(document).ready(function () {
+    $("#walletSession1").click(function (e) {
+        EnableProceedToBtn('wallet');
+    });
+    $("#walletSession2").click(function (e) {
+        EnableProceedToBtn('wallet');
+    });
+    $("#netbankingSession1").click(function (e) {
+        EnableProceedToBtn('netbanking');
+    });
+    $("#netbankingSession2").click(function (e) {
+        EnableProceedToBtn('netbanking');
+    });
+    $("#visaCardBtn").click(function (e) {
+        EnableProceedToBtn('visacard');
+    });
+    $("#rupayCardBtn").click(function (e) {
+        EnableProceedToBtn('rupaycard');
+    });
+    $('#UpiIdTextBox').on('input', function () {
+        if ($('#UpiIdTextBox')[0].value != '') {
+            EnableProceedToBtn('upi');
+        }
+    });
+});
+
+function EnableProceedToBtn(type) {
+    switch (type) {
+        case 'wallet':
+            $('#poceedToBtnOfWallet')[0].classList.remove('w-btn');
+            $('#poceedToBtnOfNetBanking')[0].classList.add('w-btn');
+            $('#poceedToBtnOfUpi')[0].classList.add('w-btn');
+            $('#rupayCardProceedBtn')[0].classList.add('w-btn');
+            $('#visaCardProceedBtn')[0].classList.add('w-btn');
+            $('#UpiIdTextBox')[0].value = '';
+            break;
+        case 'netbanking':
+            $('#poceedToBtnOfNetBanking')[0].classList.remove('w-btn');
+            $('#poceedToBtnOfWallet')[0].classList.add('w-btn');
+            $('#poceedToBtnOfUpi')[0].classList.add('w-btn');
+            $('#poceedToBtnOfWallet')[0].classList.add('w-btn');
+            $('#visaCardProceedBtn')[0].classList.add('w-btn');
+            $('#UpiIdTextBox')[0].value = '';
+            break;
+
+        case 'upi':
+            $('#poceedToBtnOfNetBanking')[0].classList.add('w-btn');
+            $('#poceedToBtnOfWallet')[0].classList.add('w-btn');
+            $('#visaCardProceedBtn')[0].classList.add('w-btn');
+            $('#rupayCardProceedBtn')[0].classList.add('w-btn');
+            $('#poceedToBtnOfUpi')[0].classList.remove('w-btn');
+            break;
+        case 'visacard':
+            $('#visaCardProceedBtn')[0].classList.remove('w-btn');
+            $('#rupayCardProceedBtn')[0].classList.add('w-btn');
+            $('#poceedToBtnOfNetBanking')[0].classList.add('w-btn');
+            $('#poceedToBtnOfWallet')[0].classList.add('w-btn');
+            $('#poceedToBtnOfUpi')[0].classList.add('w-btn');
+            $('#UpiIdTextBox')[0].value = '';
+            break;
+        case 'rupaycard':
+            $('#rupayCardProceedBtn')[0].classList.remove('w-btn');
+            $('#visaCardProceedBtn')[0].classList.add('w-btn');
+            $('#poceedToBtnOfNetBanking')[0].classList.add('w-btn');
+            $('#poceedToBtnOfWallet')[0].classList.add('w-btn');
+            $('#poceedToBtnOfUpi')[0].classList.add('w-btn');
+            $('#UpiIdTextBox')[0].value = '';
+            break;
+    }
+
+}
+
 var pageNum = 1;
 
 function proceedToPayButtonClick() {
